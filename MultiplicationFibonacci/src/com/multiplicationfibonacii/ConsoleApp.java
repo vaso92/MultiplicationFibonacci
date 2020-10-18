7dde822ddda84dcc6ef416c226f256495b19f3ab
vaso92 <vasko-vasilev@hotmail.com>
2020-10-18 15:18:11 +0300

    Initial commit for console app

---
+++ MultiplicationFibonacci/src/com/multiplicationfibonacii/ConsoleApp.java
@@ -0,0 +1,14 @@
+package com.multiplicationfibonacii;
+
+import java.util.Vector;
+
+public class ConsoleApp {
+	Vector<Long> getFibunacciMultiplicationTable(Integer n) {
+		Vector<Long> result = new Vector<Long>();
+		return result;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println("bakskfg");
+	}
+}
